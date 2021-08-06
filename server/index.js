487373f526df64ad85f3f58d300bae666174dcc6
Josh Hertz <jhertz1@gmail.com>
2021-08-05 20:23:11 -0700

    (init)

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// TODO: Create your express server here and launch on a port of your choosing
+
+  // Include a console log which prints 'Listening on port [your port] when the server is launched
+
+// Launch your server by running 'npm start' in the terminal (Why does this work? Check the package.json file to understand what this script does)
+
+// Complete the request handlers detailed in README.md
+
+// Test each route using Postman to ensure correct implementation
